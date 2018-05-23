cf3c54dc7ac70915eaed48c495788690870cc964
Keiga Sagae <sagae5.28rujeae@gmail.com>
2018-05-23 23:57:59 +0900

    feat: review.js

--- app/controllers/reviewers/review_comments_controller.rb
+++ app/controllers/reviewers/review_comments_controller.rb
@@ -3,6 +3,7 @@ class Reviewers::ReviewCommentsController < ApplicationController
   before_action :set_changed_file, only: %i(create)
   before_action :set_pull, only: %i(create)
   before_action :check_pull, only: %i(create)
+  before_action :set_review_comment, only: %i(destroy update show)
 
   def create
     reviewer = Reviewer.find(params[:reviewer_id])
@@ -16,12 +17,44 @@ class Reviewers::ReviewCommentsController < ApplicationController
       body: params[:body],
       reviewer: reviewer
     )
-    p review_comment
 
     if review_comment.save
+      render json: {
+        status: 'success',
+        review_comment_id: review_comment.id,
+        body: params[:body]
+      }
+    else
+      render json: { status: 'failed' }
+    end
+  end
+
+  def update
+    if @review_comment.update(body: params[:body])
+      render json: {
+        status: 'success',
+        body: params[:body]
+      }
+    else
+      render json: { status: 'failed' }
+    end
+  end
+
+  def destroy
+    if @review_comment.destroy
       render json: { status: 'success' }
     else
       render json: { status: 'failed' }
+    end
+  end
+
+  def show
+    respond_to do |format|
+      format.json do
+        render json: {
+          body: @review_comment.body
+        }
+      end
     end
   end
 
@@ -33,6 +66,10 @@ class Reviewers::ReviewCommentsController < ApplicationController
 
   def set_pull
     @pull = @changed_file.pull
+  end
+
+  def set_review_comment
+    @review_comment = ReviewComment.find(params[:id])
   end
 
   def check_pull
--- app/javascript/packs/reviews.js
+++ app/javascript/packs/reviews.js
@@ -11,7 +11,22 @@ $(window).on('load', function() {
   })
 
   $(document).on('click', '.review-trigger', function () {
-    startReview($(this));
+    createReviewComment($(this));
+  })
+
+  $(document).on('click', '.destroy-trigger', function () {
+    destroyReviewComment($(this));
+  })
+
+  $(document).on('click', '.edit-trigger', function () {
+    editReviewCommentForm($(this));
+  })
+
+  $(document).on('click', '.update-trigger', function () {
+    updateReviewComment($(this));
+  })
+  $(document).on('click', '.cancel-update-trigger', function () {
+    cancelUpdateReviewComment($(this));
   })
 });
 
@@ -60,11 +75,11 @@ function addForm(elem) {
         class: 'changed_file_id'
     });
     // input追加
-    $('<div class="card"><div class="card-body"></div></div>').insertAfter($(elem).closest('.code-tr'));
-    $(elem).closest('.code-tr').nextAll('.card').find('.card-body').wrapInner(input);
+    $('<div class="card card-body review-comments"></div>').insertAfter($(elem).closest('.code-tr'));
+    $(elem).closest('.code-tr').nextAll('.card').wrapInner(input);
     // @TODO widthを横幅いっぱいにする
-    $('<a class="btn btn-deep-orange cancel-trigger">Cancel</a>').insertAfter($(elem).closest('.code-tr').nextAll('.card').find('input'));
-    $('<a class="btn btn-deep-orange review-trigger">Start a review</a>').insertAfter($(elem).closest('.code-tr').nextAll('.card').find('a'));
+    $('<a class="btn btn-danger cancel-trigger">Cancel</a>').insertAfter($(elem).closest('.code-tr').nextAll('.card').find('input'));
+    $('<a class="btn btn-success review-trigger">Start a review</a>').insertAfter($(elem).closest('.code-tr').nextAll('.card').find('a'));
     positionHiddenField.insertAfter($(elem).closest('.code-tr').nextAll('.card').find('.review-trigger'));
     pathHiddenField.insertAfter($(elem).closest('.code-tr').nextAll('.card').find('.review-trigger'));
     changedFileIdHiddenField.insertAfter($(elem).closest('.code-tr').nextAll('.card').find('.review-trigger'));
@@ -77,7 +92,7 @@ function removeForm(elem) {
   elem.closest('.card').remove();
 };
 
-function startReview(elem) {
+function createReviewComment(elem) {
   $.ajax({
     type: 'POST',
     url: `/reviewers/review_comments`,
@@ -89,7 +104,79 @@ function startReview(elem) {
       body: elem.prevAll('.body').val(),
       reviewer_id: $('.data_id').attr('reviewer-id')
     },
+    element: elem,
     success: function(data) {
+      if (data.status === 'success') {
+        var card = elem.closest('.card');
+        card.empty();
+        card.wrapInner(`<p class="card-text" review-comment-id=${data.review_comment_id} />`);
+        var cardText = card.find('.card-text')
+        card.prepend('<h4 class="card-title"><span class="label label-warning">Pending</h4>')
+        cardText.text(data.body);
+        $('<a class="btn btn-success edit-trigger">UPDATE</a>').insertAfter(cardText);
+        $('<a class="btn btn-danger destroy-trigger">DELETE</a>').insertAfter(cardText);
+      }
+    }
+  });
+};
+
+function destroyReviewComment(elem) {
+  $.ajax({
+    type: 'DELETE',
+    url: `/reviewers/review_comments/${elem.prevAll('.card-text').attr('review-comment-id')}`,
+    dataType: 'JSON',
+    element: elem,
+    success: function(data) {
+      if (data.status === 'success') {
+        elem.closest('.card').prevAll('tr').find('.add-form').removeClass('add-form');
+        elem.closest('.card').remove();
+      }
+    }
+  });
+};
+
+function editReviewCommentForm(elem) {
+  var input = $('<input>').attr({
+      type: 'text_area',
+      name: 'reviews[body][]',
+      value: elem.prevAll('.card-text').text(),
+      class: 'form-control'
+  });
+  elem.prevAll('.destroy-trigger').remove();
+  $('<a class="btn btn-default cancel-update-trigger">Cancel</a>').insertAfter(elem.prevAll('p'));
+  elem.removeClass('edit-trigger').addClass('update-trigger');
+  elem.prevAll('.card-text').html(input);
+};
+
+function cancelUpdateReviewComment(elem) {
+  $.ajax({
+    type: 'GET',
+    url: `/reviewers/review_comments/${elem.prevAll('.card-text').attr('review-comment-id')}`,
+    dataType: 'JSON',
+    element: elem,
+    success: function(data) {
+      elem.removeClass('cancel-update-trigger btn-default').addClass('destroy-trigger btn-danger').text('DELETE');
+      elem.nextAll('.update-trigger').removeClass('update-trigger').addClass('edit-trigger');
+      elem.prevAll('p').find('input').remove();
+      elem.prevAll('p').text(data.body);
+    }
+  });
+};
+
+function updateReviewComment(elem) {
+  $.ajax({
+    type: 'PUT',
+    url: `/reviewers/review_comments/${elem.prevAll('.card-text').attr('review-comment-id')}`,
+    dataType: 'JSON',
+    data: { body: elem.prevAll('.card-text').find('input').val() },
+    element: elem,
+    success: function(data) {
+      if (data.status === 'success') {
+        elem.prevAll('.cancel-update-trigger').removeClass('cancel-update-trigger btn-default').addClass('destroy-trigger btn-danger').text('DELETE');
+        elem.removeClass('update-trigger').addClass('edit-trigger');
+        elem.prevAll('p').find('input').remove();
+        elem.prevAll('p').text(data.body);
+      }
     }
   });
 };
