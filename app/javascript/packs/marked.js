f8da198321e678b6dae7f93e7df197c566b1c3d5
Keiga Sagae <sagae5.28rujeae@gmail.com>
2018-08-11 00:39:47 +0900

    fix: marked.js

--- app/javascript/packs/marked.js
+++ app/javascript/packs/marked.js
@@ -9,3 +9,20 @@ $.ajax({
       marked(data.body);
   }
 });
+
+marked.setOptions({
+  // Githubっぽいmd形式にするか
+  gfm: true,
+  // Githubっぽいmdの表にするか
+  tables: true,
+  // Githubっぽいmdの改行形式にするか
+  breaks: true,
+  // Markdownのバグを修正する？（よく分からなかったので、とりあえずdefaultのfalseで）
+  pedantic: false,
+  // HTML文字をエスケープするか
+  sanitize: true,
+  // スマートなリストにするか。pedanticと関わりがあるようなので、こちらもdefaultのtrueで。
+  smartLists: true,
+  // クオートやダッシュの使い方。
+  smartypants: true,
+});
