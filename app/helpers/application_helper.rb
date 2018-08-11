580840015c71c1bcc292bb5f5719b1205de9e50f
Keiga Sagae <sagae5.28rujeae@gmail.com>
2018-08-12 01:12:50 +0900

    feat: meta

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,27 @@
 module ApplicationHelper
+  #
+  # デフォルトメタタグ設定
+  #
+  def default_meta_tags
+    {
+      site: Settings.site.name,
+      reverse: true,
+      title: Settings.site.page_title,
+      description: Settings.site.page_description,
+      keywords: Settings.site.page_keywords,
+      canonical: request.original_url,
+      og: {
+        title: :title,
+        description: Settings.site.page_description,
+        type: Settings.site.meta.ogp.type,
+        url: request.original_url,
+        image: image_url(Settings.site.meta.ogp.image_path),
+        site_name: Settings.site.name,
+        description: :description,
+        locale: 'ja_JP'
+      }
+    }
+  end
   def date_format(datetime)
     time_ago_in_words(datetime) + '前' if datetime
   end
