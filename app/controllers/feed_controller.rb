c35a2058b47cec825131ecfd4c134e81d0eaedaa
Keiga Sagae <sagae5.28rujeae@gmail.com>
2018-08-11 21:53:03 +0900

    fix: request_reviewed

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,10 +1,10 @@
 class FeedController < ApplicationController
   def index
     @skill = Skill.find_by_name(params[:skill_name])
-    @pulls = Pull.order(created_at: :desc)
+    @pulls = Pull.request_reviewed.order(created_at: :desc)
     if @skill
       repos = Repo.joins(:skillings).where(skillings: { skill_id: @skill.id })
-      @pulls = Pull.where(repo: repos).order(created_at: :desc).first(10)
+      @pulls = Pull.request_reviewed.where(repo: repos).order(created_at: :desc).first(10)
     end
     respond_to do |format|
       format.rss { render layout: false }
