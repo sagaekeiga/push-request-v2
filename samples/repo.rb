5b6af5072add17ffe2fedad9afa0cad4d7ca58a0
Keiga Sagae <sagae5.28rujeae@gmail.com>
2018-09-10 20:04:10 +0900

    feat: sample.rb

--- samples/repo.rb
+++ samples/repo.rb
@@ -6,13 +6,8 @@ headers = {
   'Accept': 'application/vnd.github.symmetra-preview+json'
 }
 
-res = HTTParty.get 'https://api.github.com/repos/sagaekeiga/push-request-v2/contents/.babelrc', headers: headers
+res = HTTParty.get 'https://api.github.com/repos/sagaekeiga/github-api-sample/pulls/31/files', headers: headers
 puts res
-# puts Base64.decode64(res['content'])
-# res.each do |file|
-#   puts file
-#   res = HTTParty.get "https://api.github.com/repos/sagaekeiga/github-api-sample/contents/#{file['path']}", headers: headers
-#   res.each do |file_2|
-#     puts file_2
-#   end
-# end
+res.each do |file|
+  puts file
+end
