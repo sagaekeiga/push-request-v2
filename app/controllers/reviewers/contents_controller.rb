2a567cfee01df2939ce541628376949ce9346beb
Keiga Sagae <sagae5.28rujeae@gmail.com>
2018-09-19 21:13:15 +0900

    feat: reviewers.contents.show

--- app/controllers/reviewers/contents_controller.rb
+++ app/controllers/reviewers/contents_controller.rb
@@ -1,10 +1,11 @@
 class Reviewers::ContentsController < Reviewers::BaseController
-  before_action :set_repo, only: %i(index)
+  before_action :set_repo, only: %i(index show)
   before_action :set_content, only: %i(show)
 
   def index
     @dir_or_files = @repo.contents.top
     @readme = @repo.contents.find_by(name: 'README.md')
+    @res_readme = Github::Request.github_exec_fetch_readme(@repo)
   end
 
   def show
@@ -19,5 +20,6 @@ class Reviewers::ContentsController < Reviewers::BaseController
 
   def set_content
     @content = Content.find(params[:id])
+    @file = Github::Request.github_exec_fetch_file_content!(@repo, @content) if @content.file?
   end
 end
