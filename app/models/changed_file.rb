fc039c05f7d24bbcbeda7e9f976d2cc020b5c90f
sagae minami <gw.feltmyheart@gmail.com>
2018-09-29 01:19:52 +0900

    fix:add params

--- app/models/changed_file.rb
+++ app/models/changed_file.rb
@@ -82,7 +82,7 @@ class ChangedFile < ApplicationRecord
     ActiveRecord::Base.transaction do
       res_diffs = Github::Request.github_exec_fetch_diff!(pull)
       commit = pull.commits.last
-      res_diffs['files'].each do |res_diff|
+      res_diffs['files']&.each do |res_diff|
         changed_file = pull.changed_files.create!(
           commit:        commit,
           event:         :compared,
